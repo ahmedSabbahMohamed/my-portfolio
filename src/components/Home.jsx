3a29a0fe095379859ac5e5e29ce6a1f6c3bfd409
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2023-08-21 18:51:22 +0300

    adding link to resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,7 +33,7 @@ function Home() {
                           ease: easeInOut,
                           stiffness: 300
                         }}
-                      href="https://docs.google.com/document/d/130sJlJumPtgv1R5bIlJCQBqSmf3ssKFj9vdNJbEew6Y/edit?usp=drive_link"
+                      href="https://drive.google.com/drive/u/1/my-drive"
                       target='_blank'
                       className='flex flex-row gap-1 items-center justify-center font-medium w-1/3 mx-auto md:mx-0 mt-3 text-lg rounded-md bg-blue-400 text-white p-2 border-2 border-blue-400 shadow-md hover:shadow-lg'
                     >
